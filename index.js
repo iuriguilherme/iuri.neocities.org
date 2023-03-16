33c303c80328ef7c98f0a2ccdc7396a75b8d4d29
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2023-03-15 21:47:21 -0300

    Adding more NFTs

--- index.js
+++ index.js
@@ -2,46 +2,97 @@
   Kopimi 2013-2023 iuri.neocities.org
   Use sem citar a fonte
 */
+var nftMap = {
+  "1": {
+    "title": "Fibonacci Rainbow Spiral v1",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmWkR6jPFmdED1tGoQ6nB7buFRtAQLPgspFNundXea5QsD/?fxhash=ooC8rtUWVcCjarygV2DSUjFjKg8oPViKBqWBLbCrgvoYKaxgmVc",
+    "img": "https://gateway.fxhash.xyz/ipfs/QmXt167vqPy1RZfYp7PmMtxfCLdBA3EW6p5oWXRhFyWqTz",
+    "url": "https://www.fxhash.xyz/generative/24631"
+  },
+  "2": {
+    "title": "Fibonacci Rainbow Spiral v2 variation 2",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmQxbjcAHqAFXreuikqu8ZeLAez1LCERGPmhzphJUUHTFd/?fxhash=ooVFcFgGXgvmDHPWV5WZTZxjNW8qVXfHY7GG7husNY7mEwnaupZ",
+    "img": "https://gateway.fxhash.xyz/ipfs/QmWHotS6RXTTi74G1ch9J5oyTpbmEpHsoHcqYGyAcJzijp",
+    "url": "https://www.fxhash.xyz/generative/24810"
+  },
+  "3": {
+    "title": "Fibonacci Rainbow Spiral v2 variation 3",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmPHYvcMffytV8u8svehH3HDFrDydEZDfkYGtcjw2bd6Rs/?fxhash=ooybyoBz3RdMJ2aKgpkCNK8HUY1dXnYshsLcPCTEf9wdKoPA5se",
+    "img": "https://gateway.fxhash.xyz/ipfs/QmaxwnuQt1oXCY2a28QuyZrW3oP19MhcEtSLsUrzXW26ph",
+    "url": "https://www.fxhash.xyz/generative/24849"
+  },
+  "4": {
+    "title": "Fibonacci Rainbow Spirals v2.1.1 var4",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmQxbjcAHqAFXreuikqu8ZeLAez1LCERGPmhzphJUUHTFd/?fxhash=ooVFcFgGXgvmDHPWV5WZTZxjNW8qVXfHY7GG7husNY7mEwnaupZ",
+    "img": "https://gateway.fxhash.xyz/ipfs/QmZidbwTAv6zsXdbmwarMhxHNCzGmeVZZHjtDQYG6bUaGq",
+    "url": "https://www.fxhash.xyz/generative/24912"
+  },
+  "5": {
+    "title": "Fibonacci Rainbow Spirals v2.1 var5",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmPdS6PcRCWyic9Kn5ehWg2ZwzAkBDhZcPjXPfm7jmH4k1/?fxhash=ooP3nRfv2FjgR75ME2Eig1T4NWQPR7qRaPDyXVicnY2Cb6vFmJh",
+    "img": "https://gateway.fxhash.xyz/ipfs/QmbXvvKsznvUtwFPKt7ii73iqik1sGqB8ieN2TnroF2Rk9",
+    "url": "https://www.fxhash.xyz/generative/24979"
+  },
+  "6": {
+    "title": "Fibonacci Rainbow Spirals v2.2 var6",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmQKotYyEVAxEi1RCnnpQ3oWbH5fU187UDA7wHNZDB1Sne/?fxhash=oofMLCYts7b43ZAsCaAUjUgz8wwuwEVRi2Fi5DPLHWQ3b2TAPMG",
+    "img": "https://gateway.fxhash.xyz/ipfs/QmTFoBxyZDbdtRdQqKJfKvxnue9E4ePnpB4ALNFzkN2X4G",
+    "url": "https://www.fxhash.xyz/generative/25018"
+  },
+  "7": {
+    "title": "Fibonacci Rainbow Spirals v2 var7",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmUxk7mb9aDdM5LciQ4a4ogHFri5MVUVt6K7NGb54JvZ3k/?fxhash=oocVygt6eLvy6GB6YCzn3YiZRCoBeQ9AZh2Ze4ZW57VTCqDkzMX",
+    "img": "https://gateway.fxhash.xyz/ipfs/QmVBnBCsaRgX5Y3rNwogpamN4E2LFPYUMFa2HVDrHyo3mj",
+    "url": "https://www.fxhash.xyz/generative/25309"
+  },
+  "8": {
+    "title": "Fibonacci Rainbow Spirals ver2 var8",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmStaUUgiCwP3i42EtpoDf71sHYh6T7NgvFZhyf9jSNBv7/?fxhash=ooBvDJCohVWfgi7vyB7fBecRqFf3p57CxnR9EueVJLh8nbJTHfx",
+    "img": "https://gateway.fxhash.xyz/ipfs/Qmcht1WhtchQDQc3Qk4Y3PBvAjJozNxSKsZmMsHUZrj2vg",
+    "url": "https://www.fxhash.xyz/generative/25381"
+  },
+  "9": {
+    "title": "Fibonacci Rainbow Spirals ver2 var9",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmNcjcUA4JFTnfmz8ggoDepXyJ6PdAdaSV3SiMaTFUBvDp/?fxhash=oooqKQNPybgwvYNo1VL5CmjPUP7XvS4aD4nmc3HeCq5fpgT71KL",
+    "img": "https://gateway.fxhash.xyz/ipfs/QmcC2B4vcVUrwRXi84soWbhHyomi3jtws2ih2yM6ofB4vW",
+    "url": "https://www.fxhash.xyz/generative/25561"
+  },
+  "10": {
+    "title": "Collatz Bezier Rainbow",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmaVEwjKubgcQbRM7DWZZH4gFNZYjyn79RRKbaXo5hRtjL/?fxhash=ooiJBerLu5LdN15HwbuQra3bPUg3vRAy6L2yXRiSrkSFRFjx97x",
+    "img": "https://gateway.fxhash.xyz/ipfs/QmXrixH1jtnUx8CT9JTGuPA1Z7tdCP11WJMvCgPXmfAMa8",
+    "url": "https://www.fxhash.xyz/generative/25615"
+  },
+  "11": {
+    "title": "Fibonacci Rainbow Spirals ver3 var10",
+    "vid": "https://gateway.fxhash2.xyz/ipfs/QmakSGgArCLfTqMkMp5mB5Y3f8RRujoiEXK8xLiv1VN2wN/?fxhash=ooF6WBPUrxnKPRpRTsb95xeqnkYTwxh9Lj5S26bAYLQ954NZmWj",
+    "img": "https://gateway.fxhash.xyz/ipfs/Qmez5bCNZRYXGec33BaP8k1Yb1Nf1tgY2zjQ7qM7Mr5yUN",
+    "url": "https://www.fxhash.xyz/generative/25935"
+  }
+};
 function toggleNft(target = 0) {
-  let nftMap = {
-    "1": {
-      "vid": "https://gateway.fxhash2.xyz/ipfs/QmWkR6jPFmdED1tGoQ6nB7buFRtAQLPgspFNundXea5QsD/?fxhash=ooC8rtUWVcCjarygV2DSUjFjKg8oPViKBqWBLbCrgvoYKaxgmVc",
-      "img": "https://gateway.fxhash.xyz/ipfs/QmXt167vqPy1RZfYp7PmMtxfCLdBA3EW6p5oWXRhFyWqTz",
-      "url": "https://www.fxhash.xyz/generative/24631"
-    },
-    "2": {
-      "vid": "https://gateway.fxhash2.xyz/ipfs/QmQxbjcAHqAFXreuikqu8ZeLAez1LCERGPmhzphJUUHTFd/?fxhash=ooVFcFgGXgvmDHPWV5WZTZxjNW8qVXfHY7GG7husNY7mEwnaupZ",
-      "img": "https://gateway.fxhash.xyz/ipfs/QmWHotS6RXTTi74G1ch9J5oyTpbmEpHsoHcqYGyAcJzijp",
-      "url": "https://www.fxhash.xyz/generative/24810"
-    },
-    "3": {
-      "vid": "https://gateway.fxhash2.xyz/ipfs/QmPHYvcMffytV8u8svehH3HDFrDydEZDfkYGtcjw2bd6Rs/?fxhash=ooybyoBz3RdMJ2aKgpkCNK8HUY1dXnYshsLcPCTEf9wdKoPA5se",
-      "img": "https://gateway.fxhash.xyz/ipfs/QmaxwnuQt1oXCY2a28QuyZrW3oP19MhcEtSLsUrzXW26ph",
-      "url": "https://www.fxhash.xyz/generative/24849"
-    },
-    "4": {
-      "vid": "https://gateway.fxhash2.xyz/ipfs/QmQxbjcAHqAFXreuikqu8ZeLAez1LCERGPmhzphJUUHTFd/?fxhash=ooVFcFgGXgvmDHPWV5WZTZxjNW8qVXfHY7GG7husNY7mEwnaupZ",
-      "img": "https://gateway.fxhash.xyz/ipfs/QmZidbwTAv6zsXdbmwarMhxHNCzGmeVZZHjtDQYG6bUaGq",
-      "url": "https://www.fxhash.xyz/generative/24912"
-    },
-    "5": {
-      "vid": "https://gateway.fxhash2.xyz/ipfs/QmPdS6PcRCWyic9Kn5ehWg2ZwzAkBDhZcPjXPfm7jmH4k1/?fxhash=ooP3nRfv2FjgR75ME2Eig1T4NWQPR7qRaPDyXVicnY2Cb6vFmJh",
-      "img": "https://gateway.fxhash.xyz/ipfs/QmbXvvKsznvUtwFPKt7ii73iqik1sGqB8ieN2TnroF2Rk9",
-      "url": "https://www.fxhash.xyz/generative/24979"
-    },
-    "6": {
-      "vid": "https://gateway.fxhash2.xyz/ipfs/QmQKotYyEVAxEi1RCnnpQ3oWbH5fU187UDA7wHNZDB1Sne/?fxhash=oofMLCYts7b43ZAsCaAUjUgz8wwuwEVRi2Fi5DPLHWQ3b2TAPMG",
-      "img": "https://gateway.fxhash.xyz/ipfs/QmTFoBxyZDbdtRdQqKJfKvxnue9E4ePnpB4ALNFzkN2X4G",
-      "url": "https://www.fxhash.xyz/generative/25018"
-    }
-  };
   for (let i = 1; i <= Object.keys(nftMap).length; i++) {
-      let iframe = document.getElementById('nft' + i);
-      iframe.src = "";
+    let iframe = document.getElementById('nft' + i);
+    iframe.src = "";
   }
   try {
     let iframe = document.getElementById('nft' + target);
     iframe.src = nftMap[target]["vid"];
+  } catch {}
+}
+
+for (let i = 1; i <= Object.keys(nftMap).length; i++) {
+  try {
+    let d = document.getElementById("nft_thumb_" + i);
+    let a = document.createElement("a");
+    let img = document.createElement("img");
+    d.className = "card text-center bg-light";
+    a.href = nftMap[i]["url"];
+    a.target = "_blank";
+    img.src = nftMap[i]["img"];
+    img.className = "card-img-top";
+    d.appendChild(a);
+    a.appendChild(img);
   } catch {}
 }
 
